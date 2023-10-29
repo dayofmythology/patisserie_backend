7836810240e1d9d7e92f67904b46998ceab7bbe4
Mohamed Ait <dayofmythology@gmail.com>
2023-10-29 11:49:38 +0000

    fix set debug to disabled + add allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-76c&7&v$x9w4ivqtoqutc%thfekuj1s6x)&4fplms8yj+!)pqw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
